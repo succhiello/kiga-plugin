902c3fea2fe197e72ab425d1b29463b7821d0158
kiri <satoshi_ebihara@xica.net>
2015-10-27 21:56:58 +0900

    fix github URL.

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     name='kiga_plugin',
     version='0.0.1',
     description='plugin loader using pkg_resources.',
-    author='xica development team',
-    author_email='info@xica.net',
-    url='https://github.com/xica/kiga-plugin',
+    author='succhiello',
+    author_email='succhiello@gmail.com',
+    url='https://github.com/succhiello/kiga-plugin',
     license='MIT',
     classifiers=[
         'Development Status :: 3 - Alpha',
